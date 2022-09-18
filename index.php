fac771701819a3ece9549e0276a6116b2804ead4
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-09-18 17:55:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 function writeMsg() {
   echo "Hello world!";
 }
-
-
+ writeMsg();
+?>
 
 <h1> Please enter your complete name and email </h1>
 <form action="handlepost.php" method="post">
@@ -13,8 +13,7 @@ E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
- writeMsg();
-  ?>
+
   </body>
 
 </html>
