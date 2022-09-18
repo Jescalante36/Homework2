4e36f618d8e5482ed7c73e8872ddae3d715426ff
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-09-18 17:17:16 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -12,6 +12,8 @@
     Your email address is: <?php echo $_POST["email"]; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
-   <br>
- <button type="button" class="btn btn-dark" onclick="location.href='http://mis4013-homework2.jescalante.oucreate.com';">Return to page</button>
+  <footer>
+<button type="button" class="btn btn-dark" onclick="location.href='http://mis4013-homework2.jescalante.oucreate.com';">Return to page</button>
+</footer>
+ 
 </html>
