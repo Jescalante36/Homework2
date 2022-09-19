ab2fc2558eed7577530723f751f7ab52beee7962
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-09-18 19:12:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,13 +6,18 @@ function CreateTitle() {
  CreateTitle();
 ?>
 
-<h1> Please enter your complete name and email </h1>
+<h1> Please enter your complete name and email if you are using POST variable </h1>
+<form action="handlepost.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit" value="Submit with Post">
+</form>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+ 
+<h1> Please enter your complete name and email if you are using GET variable </h1>
 <form action="handlepost.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
-</form>
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-
-  </body>
+ </body>
 </html>
