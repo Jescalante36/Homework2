28eb65782d1815ceaba61bbe6fe18d99a5ca87ab
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-09-18 19:27:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,9 @@ function CreateTitle() {
 <h1> Please enter your complete name and email if you are using POST variable </h1>
 <form action="handlepost.php" method="post">
 Name: <input type="text" name="name"><br>
+  <br>
 E-mail: <input type="text" name="email"><br>
+  <br>
 <input type="submit" value="Submit with Post">
 </form>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
@@ -17,7 +19,9 @@ E-mail: <input type="text" name="email"><br>
 <h1> Please enter your complete name and email if you are using GET variable </h1>
 <form action="handleget.php" method="get">
 Name: <input type="text" name="name"><br>
+  <br>
 E-mail: <input type="text" name="email"><br>
+  <br>
 <input type="submit" value="Submit with GET">
  </body>
 </html>
