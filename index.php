4b6e55ce9e30fcc10789b3d7a494492a8af28eed
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-09-18 19:13:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ E-mail: <input type="text" name="email"><br>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
  
 <h1> Please enter your complete name and email if you are using GET variable </h1>
-<form action="handlepost.php" method="post">
+<form action="handleget.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
