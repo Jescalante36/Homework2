8527fd66a4dff2bd7c43632fc22df9a58e5451c0
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-09-26 13:41:18 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -1,9 +1,6 @@
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <? require_once("header.php"); ?>
-<?php
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
